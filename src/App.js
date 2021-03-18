e827237682e3e3dacd3c3694fdf73c1d8c7576ab
duy <ddao0892@gmail.com>
2021-03-18 17:13:15 -0600

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function app() {
+  return (
+    <div className="App">
+      <h1> lets</h1>
+
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBV3MajR_JYv04utp_gjhWFYHgxPee7eXk",
+    authDomain: "whatsapp-clone-f2221.firebaseapp.com",
+    projectId: "whatsapp-clone-f2221",
+    storageBucket: "whatsapp-clone-f2221.appspot.com",
+    messagingSenderId: "619982494233",
+    appId: "1:619982494233:web:53553f63ef3832dcba6888"
+  };
